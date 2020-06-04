bf7773f65e9261a96661cb5c3a6c58a3f52384be
Yuhan <Yuhanchen8789@outlook.com>
2020-06-04 19:09:55 -0400

    general website

--- Insta/views.py
+++ Insta/views.py
@@ -1,9 +1,10 @@
+from annoying.decorators import ajax_request
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 
-from Insta.models import Post
+from Insta.models import Post, Like, InstaUser, UserConnection
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -17,9 +18,20 @@ class PostsView(ListView):
     model = Post
     template_name = 'index.html'
 
+    def get_queryset(self):
+        current_user = self.request.user
+        following = set()
+        for conn in UserConnection.objects.filter(creator=current_user).select_related('following'):
+            following.add(conn.following)
+        return Post.objects.filter(author__in=following)
+
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class UserDetailView(DetailView):
+    model = InstaUser
+    template_name = 'user_detail.html'
 
 class PostUpdateView(UpdateView):
     model = Post
@@ -31,6 +43,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     template_name = 'post_create.html'
     fields = '__all__'
     login_url = 'login'
+    
 
 class PostDeleteView(DeleteView):
     model = Post
@@ -42,3 +55,20 @@ class SignUp(CreateView):
     template_name = 'signup.html'
     success_url = reverse_lazy("login")
 
+@ajax_request
+def addLike(request):
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    try:
+        like = Like(post=post, user=request.user)
+        like.save()
+        result = 1
+    except Exception as e:
+        like = Like.objects.get(post=post, user=request.user)
+        like.delete()
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk
+    }
